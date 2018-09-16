76d2a7909232a2aa37beb77f9d52a0e747e4ecf6
Hector Mejia <caquillo722@gmail.com>
2018-09-16 16:06:35 -0400

    finished

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	// hooking to ctrl-c os signal
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("\nGood bye!")
+		// dont forget to exit
+		os.Exit(0)
+	}()
+
+	for {
+		fmt.Println("Hello there!")
+		time.Sleep(time.Second * 1)
+	}
+}
